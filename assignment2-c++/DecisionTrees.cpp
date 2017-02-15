6c78a566d278517aabea91c88826a1518f527fcc
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2017-02-15 13:53:24 +0100

    fucking working? nice dick

--- assignment2-c++/DecisionTrees.cpp
+++ assignment2-c++/DecisionTrees.cpp
@@ -4,11 +4,18 @@
 
 using namespace std;
 
-int main() {
-    string filename("data/restaurant.arff");
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        cout << "Please specify input file\n";
+        
+        return -1;
+    }
+    
+    string filename(argv[1]);
     
     DTL dtl(filename);
     Tree tree = dtl.run();
     
+    cout << "Decision tree:\n\n";
     cout << tree;
 }
