53b4f8f54b2956645a4072cb7dea2752c76e9e05
Niklas <nicko_carlsson@hotmail.com>
2017-03-01 12:33:33 +0100

    hej

--- assignment3/RobotLocalization/RobotLocalization/src/control/Main.java
+++ assignment3/RobotLocalization/RobotLocalization/src/control/Main.java
@@ -15,7 +15,7 @@ public class Main {
 			 * generate you own localiser / estimator wrapper here to plug it into the 
 			 * graphics class.
 			 */
-			EstimatorInterface l = new DummyLocalizer( 8,8, 4);
+			EstimatorInterface l = new DummyLocalizer( 6,6, 4);
 	
 			RobotLocalizationViewer viewer = new RobotLocalizationViewer( l);
 	
@@ -23,8 +23,8 @@ public class Main {
 			 * this thread controls the continuous update. If it is not started, 
 			 * you can only click through your localisation stepwise
 			 */
-			//new LocalizationDriver( 500, viewer).start();
-			new LocalizationDriver(0, viewer).start();
+			new LocalizationDriver( 500, viewer).start();
+			//new LocalizationDriver(0, viewer).start();
 		}
 	}
 }	
